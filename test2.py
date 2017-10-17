16935fcea7c566477ca5ffa4fef65ee4589561ce
Ellee Chen <ychen144@syr.edu>
2017-10-17 14:40:12 -0400

    test2

---
+++ test2.py
@@ -0,0 +1,6 @@
+Python 3.5.2 |Continuum Analytics, Inc.| (default, Jul  5 2016, 11:41:13) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 5
+>>> x
+5
+>>> 
